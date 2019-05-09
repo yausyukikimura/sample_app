b6b67e5365cb4e236bfc9babed158d1afbe28533
Yasuyuki Kimura <ky226box@gmail.com>
2019-05-09 14:17:28 +0000

    login/logout Change the display

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  include SessionsHelper
+  
+  
+  def new
+    
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      
+    else
+       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+       render 'new'
+    end
+   
+  end
+  
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
   
   
 
+  get 'sessions/new'
+
   root 'static_pages#home'
   get  "/home",    to: "static_pages#home"
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get '/signup',   to: "users#new" 
-  post "/sigup",   to: "users#create"
+  
+  get   "/login",   to: "sessions#new"
+  post   "/login",  to: "sessions#create"
+  delete"/logout",   to: "sessions#destroy"
   resources :users
  
   
