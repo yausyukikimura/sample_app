ecc27264b4c9d347139e96790652ae612e41d330
Yasuyuki Kimura <ky226box@gmail.com>
2019-05-19 05:50:34 +0000

    models/relationship.rb add  validates add belongs_to add  14.5finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,92 +1,9 @@
-# class User < ApplicationRecord
-#   attr_accessor :remember_token, :activation_token, :reset_token
-#   before_save   :downcase_email
-#   before_create :create_activation_digest
-#   validates :name,  presence: true, length: { maximum: 50 }
-#   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-#   validates :email, presence: true, length: { maximum: 255 },
-#                         format: { with: VALID_EMAIL_REGEX },
-#                         uniqueness: {case_sensitive: false}
-                        
-#     has_secure_password          
-#     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-#   class << self    
-#     # 渡された文字列のハッシュ値を返す
-#   def digest(string)
-#     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-#                                                   BCrypt::Engine.cost
-#     BCrypt::Password.create(string, cost: cost)
-#   end
-
-# # ランダムなトークンを返す
-#   def new_token
-#     SecureRandom.urlsafe_base64
-#   end
-  
-#   end
-#   def remember
-#       self.remember_token = User.new_token
-#       update_attribute(:remember_digest, User.digest(remember_token))
-#   end
-  
-  
-#   def authenticated?(attribute,token)
-#     digest = send("#{attribute}_digest")
-#     return false if digest.nil?
-#     BCrypt::Password.new(digest).is_password?(token)
-#   end
-  
-#   # ユーザーのログイン情報を破棄する
-#   def forget
-#     update_attribute(:remember_digest, nil)
-#   end
-  
-#   #アカウントを有効にする
-#   def activate
-#     update_attribute(:activated, true)
-#     update_attribute(:activated_at, Time.zone.now)
-#     #update_columns(activated: FILL_IN,activated_at: FILL_IN)
-#   end
-  
-#   # 有効化用のメールを送信する
-#   def send_activation_email
-#     UserMailer.account_activation(self).deliver_now
-#   end
-  
-#   # パスワード再設定の属性を設定する
-#   def create_reset_digest
-#     self.reset_token = User.new_token
-#     update_attribute(:reset_digest,  User.digest(reset_token))
-#     update_attribute(:reset_sent_at, Time.zone.now)
-#   end
-  
-#   #有効化用のメールを送信する
-#   def send_activation_email
-#     UserMailer.password_reset(self).deliver_now
-#   end
-  
-#   # パスワード再設定の期限が切れている場合はtrueを返す
-#   def password_reset_expired?
-#     reset_sent_at < 2.hours.ago
-#   end
-  
-#   private
-#   #アドレスを全て小文字にする
-#   def downcase_email
-#     self.email = self.email.downcase
-#   end
-  
-#   #有効以下トークンとダイジェストを作成および代入
-#   def create_activation_digest
-#     self.activation_token = User.new_token
-#     self.activation_digest= User.digest(activation_token)
-#   end
-  
-  
-# end
 
 class User < ApplicationRecord
   has_many :microposts,dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                 foreign_key: "follower_id",
+                                   dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
